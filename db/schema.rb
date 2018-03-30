2b0be18b72e6bb2532c3d9a9a07628ee69948094
acdavi <davimilt@gmail.com>
2018-03-30 00:55:05 +0000

    Sair, entrar e editar conta

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180329004049) do
+ActiveRecord::Schema.define(version: 20180330004726) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20180329004049) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "phone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
